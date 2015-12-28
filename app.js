35a142ab17a566582f1405d062a9f21ecf6f0b3d
Ramzy Karam <Ramzy.karam92@gmail.com>
2015-12-28 16:49:01 +0200

    tested the website app.js with the json data

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 var goalsSection = document.getElementById('goals');
-var index = 1;
-var title = "Hello New Year";
-var mygoals = [
-  "love new year",
-  "have six packs",
-  "eat well"
-];
-var mystate = [
-  0,
-  1,
-  0
-];
-goalsSection.innerHTML = goalsTemplate(index, title, mygoals, mystate);
+for(var j = 0 ; j < mygoals.length; j++){
+  goalsSection.innerHTML = goalsSection.innerHTML + goalsTemplate(j+1, mygoals[j].title, mygoals[j].goals, mygoals[j].state);
+}
